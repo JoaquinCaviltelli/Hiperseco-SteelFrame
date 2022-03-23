f37c61ec42750b378f29a49c7e4b649dba8dd9fe
Joaquin <joakindel95@gmail.com>
2022-03-23 00:09:27 -0300

    commit de prueba

---
+++ src/views/Paneles.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="box">
+        <router-link to="/">Panel Exterior</router-link>
+      </div>
+      <div class="box">
+        <router-link to="/">Panel Interior</router-link>
+      </div>
+      <div class="box">
+        <router-link to="/">Panel Exterior - Exterior</router-link>
+      </div>
+      <div class="box">
+        <router-link to="/">Cubierta</router-link>
+      </div>
+      <div class="box">
+        <router-link to="/">Viga Tubo</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Paneles",
+  components: {},
+};
+</script>
+
+<style scoped>
+.container {
+  margin-left: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+.box {
+  width: 300px;
+  border: 3px solid orange;
+  border-radius: 5px;
+  padding: 15px;
+}
+a {
+  text-decoration: none;
+  font: 16px;
+  color: #666666;
+}
+</style>
